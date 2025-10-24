9f553ba0084e8b5f8c8885f43058fd9ce0134e00
Erik-Hard1 <13EH1104@amal.se>
2025-10-24 09:52:40 +0200

    sadsadsadsadsadsad

--- Main.cs
+++ Main.cs
@@ -9,19 +9,27 @@ public partial class Main : Node
 	public Node GameScene;
 	public CanvasLayer TitleScreen;
 	public CanvasLayer DeathScreen;
+	public CanvasLayer PauseScreen;
+
+	public bool paused = false;
 	// Called when the node enters the scene tree for the first time.
-	
+
 	public override void _Ready()
 	{
 		GameScene = GetNode<Node>("Game");
 		TitleScreen = GetNode<CanvasLayer>("MainMenu");
 		DeathScreen = GetNode<CanvasLayer>("DeathScreen");
+		PauseScreen = GetNode<CanvasLayer>("PauseMenu");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
-	}
+    {
+        if (Input.IsActionJustPressed("Pause"))
+        {
+			PauseButtonPressed();
+        }
+    }
 
 	public void OnPlayButtonPressed()
 	{
@@ -34,9 +42,29 @@ public partial class Main : Node
 	{
 		DeathScreen.Visible = true;
 	}
-	
+
 	public void RestartButtonPressed()
-    {
-        GetTree().CallDeferred("reload_current_scene");
-    }
+	{
+		GetTree().CallDeferred("reload_current_scene");
+	}
+
+	public void PauseButtonPressed()
+	{
+		if (DeathScreen.Visible == false && TitleScreen.Visible == false)
+		{
+			if (!paused)
+			{
+				GameScene.GetTree().Paused = true;
+				PauseScreen.Visible = true;
+				paused = true;
+			}
+			else
+			{
+				GameScene.GetTree().Paused = false;
+				PauseScreen.Visible = false;
+				paused = false;
+			}
+		}
+
+	}
 }
